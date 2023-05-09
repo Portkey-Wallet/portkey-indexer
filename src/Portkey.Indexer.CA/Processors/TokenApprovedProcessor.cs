c1f06d457056f060a0eba67a2dc5c480c5fdeabb
kevin <kevin@portkey.finance>
2023-05-09 13:54:31 +0800

    feat: add TokenApprovedProcessor

---
+++ src/Portkey.Indexer.CA/Processors/TokenApprovedProcessor.cs
@@ -0,0 +1,57 @@
+using AElf.Contracts.MultiToken;
+using AElfIndexer.Client;
+using AElfIndexer.Client.Handlers;
+using AElfIndexer.Grains.State.Client;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Portkey.Indexer.CA.Entities;
+using Volo.Abp.ObjectMapping;
+
+namespace Portkey.Indexer.CA.Processors;
+
+public class TokenApprovedProcessor : CAHolderTransactionProcessorBase<Approved>
+{
+    public TokenApprovedProcessor(ILogger<TokenApprovedProcessor> logger,
+        IAElfIndexerClientEntityRepository<CAHolderIndex, LogEventInfo> caHolderIndexRepository,
+        IAElfIndexerClientEntityRepository<CAHolderManagerIndex, LogEventInfo> caHolderManagerIndexRepository,
+        IAElfIndexerClientEntityRepository<CAHolderTransactionIndex, TransactionInfo>
+            caHolderTransactionIndexRepository,
+        IAElfIndexerClientEntityRepository<TokenInfoIndex, LogEventInfo> tokenInfoIndexRepository,
+        IAElfIndexerClientEntityRepository<NFTInfoIndex, LogEventInfo> nftInfoIndexRepository,
+        IAElfIndexerClientEntityRepository<CAHolderTransactionAddressIndex, TransactionInfo>
+            caHolderTransactionAddressIndexRepository,
+        IOptionsSnapshot<ContractInfoOptions> contractInfoOptions,
+        IOptionsSnapshot<CAHolderTransactionInfoOptions> caHolderTransactionInfoOptions, IObjectMapper objectMapper) :
+        base(logger, caHolderIndexRepository, caHolderManagerIndexRepository, caHolderTransactionIndexRepository,
+            tokenInfoIndexRepository,
+            nftInfoIndexRepository, caHolderTransactionAddressIndexRepository, contractInfoOptions,
+            caHolderTransactionInfoOptions, objectMapper)
+    {
+    }
+
+    public override string GetContractAddress(string chainId)
+    {
+        return ContractInfoOptions.ContractInfos.First(c => c.ChainId == chainId).CAContractAddress;
+    }
+
+    protected override async Task HandleEventAsync(Approved eventValue, LogEventContext context)
+    {
+        if (!IsValidTransaction(context.ChainId, context.To, context.MethodName, context.Params)) return;
+        var holder = await CAHolderIndexRepository.GetFromBlockStateSetAsync(IdGenerateHelper.GetId(context.ChainId,
+            eventValue.Owner.ToBase58()), context.ChainId);
+        if (holder == null) return;
+        var index = new CAHolderTransactionIndex
+        {
+            Id = IdGenerateHelper.GetId(context.BlockHash, context.TransactionId),
+            Timestamp = context.BlockTime.ToTimestamp().Seconds,
+            FromAddress = eventValue.Owner.ToBase58(),
+            TransactionFee = GetTransactionFee(context.ExtraProperties)
+        };
+        ObjectMapper.Map(context, index);
+        index.MethodName = GetMethodName(context.MethodName, context.Params);
+        await CAHolderTransactionIndexRepository.AddOrUpdateAsync(index);
+        await AddCAHolderTransactionAddressAsync(holder.CAAddress, eventValue.Spender.ToBase58(), context.ChainId,
+            context);
+    }
+}
