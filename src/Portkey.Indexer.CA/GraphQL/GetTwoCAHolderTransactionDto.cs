c4b5cac538318dd078cb7d149f6e78132fcde0f2
felix-zhao <123852192+felix-zhaolei@users.noreply.github.com>
2023-05-26 11:05:03 +0800

    Add two caholder transactions method (#8)
    feat: add twoCAHolderTransaction method and remove some add CAHolderTransactionAddress method.

---
+++ src/Portkey.Indexer.CA/GraphQL/GetTwoCAHolderTransactionDto.cs
@@ -0,0 +1,27 @@
+using GraphQL;
+using Volo.Abp.Application.Dtos;
+
+namespace Portkey.Indexer.CA.GraphQL;
+
+public class GetTwoCAHolderTransactionDto : PagedResultRequestDto
+{
+    public string ChainId { get; set; }
+
+    public long StartBlockHeight { get; set; }
+
+    public long EndBlockHeight { get; set; }
+
+    public string Symbol { get; set; }
+    
+
+    [Name("caAddressInfos")]
+    public List<CAAddressInfo> CAAddressInfos { get; set; }
+
+    public string TransactionId { get; set; }
+
+    public string BlockHash { get; set; }
+
+    public string TransferTransactionId { get; set; }
+
+    public List<string> MethodNames { get; set; } = new();
+}
